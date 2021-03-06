f4b0fe11e3dc85e567bacf7e514ba71d32f3b3c7
chadanhuggup <48266234+chadanhuggup@users.noreply.github.com>
2021-03-06 12:10:00 -0500

    Created Payment Class

---
+++ src/domain/Payment.java
@@ -0,0 +1,46 @@
+package domain;
+
+public class Payment {
+	private Customer customer;
+	private double amount;
+	private Date paymentDate;
+	
+	public Payment(Customer customer, double amount, Date paymentDate) {
+		this.customer = customer;
+		this.amount = amount;
+		this.paymentDate = paymentDate;
+	}
+	
+	//accessors/getters
+	public Customer getCustomer() {
+		return customer;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public Date getPaymentDate() {
+		return paymentDate;
+	}
+	
+	//mutators/setters
+	public void setCustomer(Customer customer) {
+		this.customer= customer ;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public void setPaymentDate(Date paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	
+	//print function
+		//toString print method
+		public String toString() {
+			String out;
+			out=("\n********************\nCustomer: " + getCustomer() + "\n");
+			out+=("Amount: " + getAmount() + "\n");
+			out+=("Date of payment: " + getPaymentDate().getDay() + "/"
+			+ getPaymentDate().getMonth() + "/" + getPaymentDate().getYear() + "\n********************\n");
+			return out;
+		}
+}
