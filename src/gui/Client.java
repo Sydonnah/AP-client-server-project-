de9852ec32e516b9a27197db36f5fc486286408d
chadanhuggup <48266234+chadanhuggup@users.noreply.github.com>
2021-04-10 10:32:20 -0500

    Added logging for server class

--- src/gui/Client.java
+++ src/gui/Client.java
@@ -144,6 +144,8 @@ public class Client extends javax.swing.JFrame
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+    	
+    	logger.info("Live chat loaded successfully");
 
 
         lb_address = new javax.swing.JLabel();
